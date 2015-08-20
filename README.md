839de5c8c3c1f980a07f39844c823c991ece294a
JB <jbrilla17@gmail.com>
2015-08-20 18:00:31 +1000

    tightened up code yo

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ### Play with a friend or with a computer that guesses at random.
 
+### If even has keyboard functions.
+- "ESC" to reset board.
+- "SPACEBAR" to make random guess. 
+
 
 ##Specs
 - jquery 1.11.3
--- js/tictactoe.js
+++ js/tictactoe.js
@@ -3,16 +3,16 @@ var Tictactoe = {
     PlayerOne: 0,
     PlayerTwo: 0,
 
-    //Xs and Os are inserted here.
+    //Xs and Os are inserted here. Virtual Board
     tictactoeBoard: [
         [null, null, null],
         [null, null, null],
         [null, null, null]
     ],
-
+    // Pushes empty square locations here.
     emptySquares: [],
 
-    //locations from tictactoeNumbers are added here.
+    //Collection of player locations from tictactoeNumbers.
     playerOneCollect: [],
     playerTwoCollect: [],
 
@@ -33,8 +33,11 @@ var Tictactoe = {
         [11, 22, 33],
         [13, 22, 31]
     ],
+    
     //Whos turn.
     turnX: true,
+    
+    //for draw
     winner: false,
 
     //Resets the board.
@@ -43,8 +46,8 @@ var Tictactoe = {
         for (var i = 0; i < Tictactoe.tictactoeBoard.length; i++) {
 
             for (var j = 0; j < Tictactoe.tictactoeBoard[i].length; j++) {
+                
                 Tictactoe.tictactoeBoard[i][j] = null;
-                //console.log(Tictactoe.tictactoeBoard[i][j])
 
             }
         }
@@ -70,7 +73,7 @@ var Tictactoe = {
         var row = parseInt(splitId[0]) - 1;
         var colum = parseInt(splitId[1]) - 1;
 
-        //If square used
+        //If square is not empty
         if (Tictactoe.tictactoeBoard[row][colum] === 'X' || Tictactoe.tictactoeBoard[row][colum] === 'O') {
             return console.log('please choose again, square is already taken', row, colum);
         }
@@ -78,13 +81,14 @@ var Tictactoe = {
         //sets player to X or O
         var player = (Tictactoe.turnX === true) ? 'X' : 'O';
 
+        //adds to board array
         Tictactoe.tictactoeBoard[row][colum] = player;
 
-        //Get which player and pushes value to it.
+        //gets which player and pushes move value to it.
         var tttPlayerPoints = (player === "X") ? Tictactoe.playerOneCollect : Tictactoe.playerTwoCollect
         tttPlayerPoints.push(Tictactoe.tictactoeNumbers[row][colum]);
 
-        //Places player of html
+        //Places player on html
         $('#' + id).text(player);
 
         //winner?
@@ -97,23 +101,22 @@ var Tictactoe = {
 
         }
 
-        //Sets player for next turn.
+        //sets up next player turn.
         Tictactoe.turnX = (player === 'X') ? false : true;
         $('#message').text((Tictactoe.turnX === true) ? 'X' : 'O');
         var playerCollect = (player === 'X') ? Tictactoe.playerOneCollect : Tictactoe.playerTwoCollect;
         
 
-
+        //resets array
         Tictactoe.emptySquares = [];
         
-        //Checks for whats empty
+        //checks for whats empty after turn.
         for (var i = 0; i < Tictactoe.tictactoeBoard.length; i++) {
             for (var j = 0; j < Tictactoe.tictactoeBoard[i].length; j++) {
 
                 if ( Tictactoe.tictactoeBoard[i][j] === 'X' || Tictactoe.tictactoeBoard[i][j] === 'O' ) {
                     
                 } else {
-                    //console.log(Tictactoe.tictactoeNumbers[i][j]);
 
                     Tictactoe.emptySquares.push(Tictactoe.tictactoeNumbers[i][j]);
                 }
@@ -123,24 +126,19 @@ var Tictactoe = {
         }
 
         console.log('Player:', player,' ID: ', id, 'Position:', row, colum, 'Squares Collected:', playerCollect );
-        //Tictactoe.computerAI();
-        
-
 
     },
     // CHECK FOR WINNER
     checkForWinner: function() {
-        //Checks for draw.
-        
-
-        
         
         var playerOne = Tictactoe.playerOneCollect;
         var playerTwo = Tictactoe.playerTwoCollect;
 
+        //gets current player
         var currentPlayer = (Tictactoe.turnX === true) ? Tictactoe.playerOneCollect : Tictactoe.playerTwoCollect;
         var player = (Tictactoe.turnX === true) ? 'X' : 'O';
 
+        //cycles through winning combinations
         for (var i = 0; i < Tictactoe.winningCombo.length; i++) {
 
             $winningCombo = $(Tictactoe.winningCombo[i]);
@@ -153,7 +151,6 @@ var Tictactoe = {
                 Tictactoe.winner = true;
 
                 
-                
                 return alert(player + ' Wins!');
                 //Tictactoe.resetBoard();
 
@@ -162,14 +159,16 @@ var Tictactoe = {
         }
 
     },
-
+    //Add to player score - onscreen and in js var.
     addToScore: function(x) {
 
         if (x === "X") {
+            
             Tictactoe.PlayerOne += 1
             $('#playerone').text(Tictactoe.PlayerOne);
     
         } else {
+            
             Tictactoe.PlayerTwo += 1;
             $('#playertwo').text(Tictactoe.PlayerTwo);
             
@@ -183,50 +182,56 @@ var Tictactoe = {
         var turn = Tictactoe.turnX;
         var numbers = Tictactoe.tictactoeNumbers
 
-            var a = $(Tictactoe.emptySquares).not(Tictactoe.winningCombo[0]).get()
-            console.log($(Tictactoe.emptySquares).not(a).get());
-            
-            var b = Math.floor(Math.random() * Tictactoe.emptySquares.length)+1;
-             
-            var c = [22, 11, 13, 33, 31]
-            var d = Math.floor(Math.random() * c.length)+1;
-            if ( Tictactoe.emptySquares.length === 0 ) {
-                Tictactoe.chooseSquare((22).toString(), false);
-            } else {
-                Tictactoe.chooseSquare((Tictactoe.emptySquares[(b-1)]).toString(), false);
-            }
 
-            // if (board[1][1]) {
+        //var a = $(Tictactoe.emptySquares).not(Tictactoe.winningCombo[0]).get()
+        //console.log($(Tictactoe.emptySquares).not(a).get());
+        
+        //gets random empty square
+        var b = Math.floor(Math.random() * Tictactoe.emptySquares.length)+1;
+         
+        // var c = [22, 11, 13, 33, 31]
+        // var d = Math.floor(Math.random() * c.length)+1;
 
-            // }
+        if ( Tictactoe.emptySquares.length === 0 ) {
+
+            Tictactoe.chooseSquare((22).toString(), false);
+
+        } else {
+
+            Tictactoe.chooseSquare( (Tictactoe.emptySquares[(b-1)]).toString(), false );
+
+        }
 
     },
 
     //click functions
     init: function() {
-
+        //hover for buttons on board
         $('td').hover(function() {
-                $(this).attr('class', 'squareRed')
-            },
-            function() {
-                $(this).attr('class', 'square')
-            });
 
-        //RESETS BOARD
+                $(this).attr('class', 'squareRed')
+
+        }, function() {
+
+                $(this).attr('class', 'square')
+
+        });
+
+        //RESETS BOARD - esc key & button
         $('#reset').on('click', Tictactoe.resetBoard);
-        $(document).on('keyup',function(evt) {
-            if (evt.keyCode == 27) {
+        $(document).on('keyup',function(e) {
+            if (e.keyCode == 27) {
                Tictactoe.resetBoard();
             }
         });
 
 
-        //COMPUTER PLAY
+        //COMPUTER PLAY - spacebar & button
         $('#computer').on('click', function (e) {
             Tictactoe.computerAI();
         });
-        $(document).on('keyup',function(evt) {
-            if (evt.keyCode == 32) {
+        $(document).on('keyup',function(e) {
+            if (e.keyCode == 32) {
                Tictactoe.computerAI();
             }
         });
@@ -243,9 +248,13 @@ var Tictactoe = {
 
 // The only thing waiting for the DOM is .init();
 $(document).ready(function() {
+
     Tictactoe.init();
-    console.log('Game Loaded');
+    
     $('#message').text((Tictactoe.turnX === true) ? 'X' : 'O');
+    
     $('td').empty();
+
+     console.log('Game Loaded');
 
 });
